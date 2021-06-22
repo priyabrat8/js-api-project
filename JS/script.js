9538399441a979a41a3ef75cc9bbece6b473ebff
Priyabrat <PriyabratSwain888@gmail.com>
2021-06-22 22:02:18 +0530

    Initial Commit

---
+++ JS/script.js
@@ -0,0 +1,48 @@
+// ->https://icanhazdadjoke.com/
+
+const jokes = document.querySelector("#joke");
+const jokeBtn = document.querySelector("#jokeBtn"); 
+
+//    1->Promises 
+// const  generateJokes = () => {
+//     const setHeader = {
+//         headers:{
+//             Accept : "application/json"
+//         }
+//     }
+    
+//     fetch('https://icanhazdadjoke.com',setHeader)
+     
+//     .then((res) => res.json() )
+    
+//     .then((data) => {
+//         jokes.innerHTML = data.joke;
+//     })
+
+//     .catch((err) => {
+//         console.log(err);
+//     })
+// };
+
+    // 2->Async-Await
+const  generateJokes = async () => {
+
+    try{
+        const setHeader = {
+            headers:{
+                Accept : "application/json"
+            }
+        }
+        
+        const res = await fetch('https://icanhazdadjoke.com',setHeader)
+        const data  = await res.json()
+        jokes.innerHTML = data.joke;
+    }
+    
+    catch(err){
+        console.log(err);
+    }
+};
+
+jokeBtn.addEventListener('click',generateJokes);
+generateJokes();
